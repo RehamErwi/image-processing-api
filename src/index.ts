da4c4d43bced982f6a69fba80c9f62c97922d840
Reham Erwi <rehamerwi@Rehame.local>
2022-10-21 16:39:38 +0300

    index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-const myFunc = (num: number): number => {
-  return num * num;
-};
+import cors from 'cors';
+import express from 'express';
+import { Logger } from './middleware/app';
 
-export default myFunc;
+const app = express();
+const port = 3000;
+const middleware = [cors(), Logger];
+
+app.use(middleware);
+
+app.listen(port, () => {
+    return console.log(`Hey, hey, server is listening on ${port} `);
+});
+
+export default app;
