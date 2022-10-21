78f7c9e464f0324c852ba19464224d5e45fb016f
Reham Erwi <rehamerwi@Rehame.local>
2022-10-21 16:46:46 +0300

    use image route

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import cors from 'cors';
 import express from 'express';
 import { Logger } from './middleware/app';
+import images from './routes/api/images';
+import routes from './routes';
 
 const app = express();
 const port = 3000;
 const middleware = [cors(), Logger];
 
 app.use(middleware);
+
+routes.use('/images', images);
 
 app.listen(port, () => {
     return console.log(`Hey, hey, server is listening on ${port} `);
